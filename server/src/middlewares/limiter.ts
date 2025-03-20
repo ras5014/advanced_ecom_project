c3a73df1e91fb0c428937a6c12c896f0c4512659
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2025-03-20 21:57:31 +0530

    `Added two log messages at the end of the file, including server startup information.`

--- server/src/middlewares/limiter.ts
+++ server/src/middlewares/limiter.ts
@@ -5,7 +5,7 @@ import redisClient from "../lib/redis";
 export const limiter = rateLimit({
   // Rate limiter configuration
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: {
